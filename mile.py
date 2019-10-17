dd1b59da15ce6ca3fe3cf6e83f87158c95fd28f4
Tomas Medina <tomymg11@hotmail.com>
2019-10-17 14:54:43 -0300

    python convertor

---
+++ mile.py
@@ -0,0 +1,9 @@
+print("hello, I'am a convertor, type a number of km and I say to you in miles")
+
+kms = input()
+
+miles = float(kms)/1.09
+
+miles = round(miles, 2)
+
+print(f"thast is {miles} miles")
